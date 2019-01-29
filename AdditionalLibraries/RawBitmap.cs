058dc91bd755dd7775e6981da73b7f33f88494e5
3x3y3z3t <>
2019-01-30 00:41:37 +0700

    v2.1 initial commit

---
+++ AdditionalLibraries/RawBitmap.cs
@@ -0,0 +1,114 @@
+﻿// ;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Stealer.AdditionalLibraries
+{
+    class RawBitmap
+    {
+        public enum Type { Raw_FromScreen }
+        public byte[] RawData { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public Bitmap Original { get; private set; }
+
+        public RawBitmap(int _x, int _y, int _w, int _h, Type _type = Type.Raw_FromScreen)
+        {
+            Bitmap Original = new Bitmap(_w, _h);
+            Graphics.FromImage(Original).CopyFromScreen(_x, _y, 0, 0, new Size(_w, _h), CopyPixelOperation.SourceCopy);
+            BitmapData data = Original.LockBits(new Rectangle(0, 0, _w, _h), ImageLockMode.ReadOnly, Original.PixelFormat);
+            RawData = new byte[_w * _h];
+            Marshal.Copy(data.Scan0, RawData, 0, _w * _h);
+            Width = data.Width;
+            Height = data.Height;
+
+            Original.UnlockBits(data);
+        }
+
+        public Point Find(byte[] _needle, int _w, int _h)
+        {
+            if (_w > Width)
+                return new Point(-1, -1);
+
+            int i = 0, j = 0, x = -1;
+            while (i < Height)
+            {
+                int xx = x;
+                xx = Find(i * Width + (x < 0 ? 0 : x), _needle, j * _w, _w);
+                if (x < 0)
+                {
+                    x = xx;
+                }
+                if (xx >= 0)
+                    ++j;
+                else
+                    j = 0;
+
+
+                if (j >= _h)
+                    return new Point(x, i);
+
+                ++i;
+            }
+
+            return new Point(-1, -1);
+
+
+        }
+
+        private int Find(int _offs, byte[] _needle, int _noffs, int _nlength)
+        {
+            int i = 0, j = 0;
+            while (i + 4 <= _nlength)
+            {
+                if (RawData[_offs + i + 0] == _needle[_noffs * j + 0] && // A;
+                    RawData[_offs + i + 1] == _needle[_noffs * j + 1] && // R;
+                    RawData[_offs + i + 2] == _needle[_noffs * j + 2] && // G;
+                    RawData[_offs + i + 3] == _needle[_noffs * j + 3])   // B;
+                {
+                    ++j;
+                }
+                else
+                {
+                    j = 0;
+                }
+                if (j >= _nlength)
+                    return i;
+                ++i;
+            }
+            return -1;
+        }
+
+        private int Find(int[] _haystack, int[] _needle)
+        {
+            if (_haystack.Length < _needle.Length)
+                return -1;
+
+            int i = 0, j = 0;
+            while (i + _needle.Length <= _haystack.Length)
+            {
+                if (_haystack[i] == _needle[j])
+                {
+                    ++j;
+                }
+                else
+                {
+                    j = 0;
+                }
+
+                if (j >= _needle.Length)
+                {
+                    return i;
+                }
+
+
+
+                ++i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ StealerClicker.cs
@@ -0,0 +1,216 @@
+﻿// ;
+using Exw.ClickerCore;
+using Stealer.AdditionalLibraries;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Stealer
+{
+    public class StealerClicker : Clicker
+    {
+        #region Win32 API Imports
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        public struct PCURSORINFO
+        {
+            public int cbSize;
+            public int flag;
+            public IntPtr hCursor;
+            public POINT ptScreenPos;
+        }
+
+        [DllImport("User32.dll")]
+        public static extern bool GetCursorInfo(out PCURSORINFO _pci);
+        #endregion
+
+        public bool StealMore { get; set; }
+
+        private bool firsttime;
+
+        private Point escPoint;
+
+        protected override void Init()
+        {
+            base.Init();
+            GlobalOffset = new Point(0, 0);
+            TargetAppName = "Epic Battle Fantasy 5";
+
+            // TODO: add firsttime;
+
+            // 63 x 49;
+            // x = 1        1250    1
+            // y = 1    25  699     1
+
+            StealMore = false;
+            firsttime = false;
+            escPoint = new Point(540, 560);
+
+
+
+
+#if true
+            // init battle: (375, 249, 437, 298);
+            AddMove(406, 274);
+            AddClick(0U);
+            AddDelay(2200);
+
+            // move to "Skill" button: (299, 505, 433, 541);
+            AddMove(366, 523);
+            AddClick(() => { return firsttime; }, 0U);
+
+            // move to "Recently Used" area, or move to scan skill;
+            AddMove(() =>
+            {
+                if (firsttime)
+                {
+                    // TODO: scan skill;
+                    return new Point(0, 0);
+                }
+                else
+                {
+                    return new Point(490, 600);
+                }
+            });
+            AddClick(0U);
+
+            // move to foe 1: ();
+            AddMove(1018, 156);
+            AddClick();
+            AddDelay(1900);
+
+            // move to "Limit" button: (299, 429, 433, 465);
+
+            // move to "Recently Used" area, or move to scan skill;
+            AddMove(() =>
+            {
+                if (StealMore && firsttime)
+                {
+                    // TODO: scan skill;
+                    return new Point(0, 0);
+                }
+                else
+                {
+                    return new Point(490, 600);
+                }
+
+            });
+            AddClick(() => { return StealMore; }, 0U);
+
+            // move to foe 2: ();
+            AddMove(1068, 331);
+            AddClick(() => { return StealMore; });
+            AddDelay(() =>
+            {
+                if (StealMore)
+                    return 1900;
+                return 0;
+            });
+
+            // move to "Tactics" button: (299, 467, 433, 503);
+            AddMove(366, 485);
+            AddClick(0U);
+
+            // move to "Escape": ();
+            AddMove(() =>
+            {
+                if (firsttime)
+                {
+                    // TODO: scan Ecscape;
+                    // 446, 339, 580, 595 (135 x 257);
+                    escPoint = new Point(0, 0);
+                }
+                return escPoint;
+            });
+            AddClick(0U);
+
+            // move to party: ();
+            AddMove(252, 256);
+            AddClick(0U);
+            AddDelay(4300);
+
+            //AddCustom(() => { Pause(); });
+            //AddCustom(() =>
+            //{
+            //// failsafe;
+            //if (firsttime) {
+            //    firsttime = false;
+            //    Remove(3);
+            //    Remove(3);
+            //    Remove(3);
+            //    Remove(Sequence.Count - 1);
+            //}
+            //});
+#else
+            RawBitmap rbm = new RawBitmap(466, 399, 135, 257);
+            
+            // this should spam cursor on the four corner;
+            int x1 = 299, y1 = 467;
+            int x2 = 433, y2 = 503;
+            //AddMove(() =>
+            //{
+            //    if (firsttime)
+            //    {
+            //        firsttime = false;
+            //        return new Point(x1, y1);
+            //    }
+            //    else
+            //    {
+            //        firsttime = true;
+            //        return new Point(0, 0);
+            //    }
+            //});
+            AddMove(x1, y1);
+            AddDelay(500);
+            AddMove(x2, y1);
+            AddDelay(500);
+            AddMove(x2, y2);
+            AddDelay(500);
+            AddMove(x1, y2);
+            AddDelay(500);
+
+
+            Bitmap stealitem;
+            
+
+#endif
+
+            //firsttime = true;
+
+        }
+
+        protected override void OnRunning()
+        {
+            base.OnRunning();
+
+
+
+            UpdateAppStatus(true);
+            //Remove(1);
+        }
+
+        protected override bool ShouldClick()
+        {
+            PCURSORINFO pci;
+            pci.cbSize = Marshal.SizeOf(typeof(PCURSORINFO));
+            if (GetCursorInfo(out pci))
+            {
+                //Console.WriteLine(pci.hCursor == (IntPtr)65567);
+                //return (pci.hCursor == (IntPtr)65567);
+                if (pci.hCursor == (IntPtr)65567)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
